90d128b17faccc09f76902828a814b7f8c74389a
ndemeyvan <ndemeyvan@gmail.com>
2022-08-21 12:42:01 +0000

    delete blog with fetch request

--- src/BlogDetail.jsx
+++ src/BlogDetail.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useGetFecth from "./useGetFecth";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetail = () => {
+  const [isDeleteLoading, setisDeleteLoading] = useState(false);
+  //   Manual redirection
+  const history = useHistory();
 
   const { id } = useParams();
   const {
@@ -11,7 +15,29 @@ const BlogDetail = () => {
     error,
   } = useGetFecth("http://localhost:8000/blogs/" + id);
 
-  const handleDelete = (id) => {};
+  const handleDelete = (id) => {
+    setisDeleteLoading(true);
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs/" + id, {
+        method: "DELETE",
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Request Failed , please try again ...");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log("new Blog add " + data);
+          setisDeleteLoading(false);
+          history.push("/");
+        })
+        .catch((e) => {
+          console.log("This is request error ", e.message);
+          setisDeleteLoading(false);
+        });
+    }, 1000);
+  };
 
   return (
     <div className="blog-details">
@@ -22,7 +48,10 @@ const BlogDetail = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <p>{blog.body}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete</button>
+          {isDeleteLoading && <button>Deleting ... </button>}
+          {!isDeleteLoading && (
+            <button onClick={() => handleDelete(blog.id)}>Delete</button>
+          )}
         </article>
       )}
     </div>
