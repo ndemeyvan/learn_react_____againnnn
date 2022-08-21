1f0f1b48c188143ac2fe53490124912ddac9356c
ndemeyvan <ndemeyvan@gmail.com>
2022-08-21 11:09:02 +0000

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
 import Home from "./home";
 import NavBar from "./navBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateBlog from "./create";
 
 class App extends Component {
   render() {
-
     return (
-      <div className="App">
-        <NavBar/>
-        <div className="content">
-          <Home/>
-        
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div className="content">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/create">
+                <CreateBlog />
+              </Route>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/create.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateBlog = () => {
+  return (
+    <div className="create">
+      <h2>Create a new Article </h2>
+    </div>
+  );
+};
+
+export default CreateBlog;
